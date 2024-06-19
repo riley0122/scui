04d97993d32b7a9d3d8bcdd2bb60a54b5a04ee69
Riley0122 <77288737+riley0122@users.noreply.github.com>
2024-06-19 16:03:51 +0200

    Add colour to the testing library

--- Test/test_lib.hpp
+++ Test/test_lib.hpp
@@ -9,11 +9,11 @@ int run_tests(std::vector<bool> tests, std::vector<std::string> testNames) {
 	for (long unsigned int i = 0; i < tests.size(); i++)
 	{
 		if (!tests[i]) {
-			std::cout << "[FAILED] test: " << testNames[i] << std::endl;
+			std::cout << "\x1b[31;49;1m[FAILED]\x1b[0m test: " << testNames[i] << std::endl;
 			failed = true;
 		}
 		else {
-			std::cout << "[PASSED] test: " << testNames[i] << std::endl;
+			std::cout << "\x1b[32;49;1m[PASSED]\x1b[0m test: " << testNames[i] << std::endl;
 		}
 	}
 
